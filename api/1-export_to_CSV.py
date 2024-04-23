1e9291ea5a7f5bb430dbae186b405c2689f5e513
heydarov93 <heyderov.yashar@mail.ru>
2024-04-23 12:11:20 +0400

    execution guard added for import case

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -4,34 +4,35 @@ Script that, using REST API, for a given employee ID,
 returns information about his/her TODO list progress
 """
 
+import json
 from sys import argv
 from urllib import request
-import json
 
 
-employee_id = argv[1]
+if __name__ == "__main__":
+    employee_id = argv[1]
 
-todos_json = request.urlopen('https://jsonplaceholder.typicode.com/' +
-                             f'todos?userId={employee_id}')
+    todos_json = request.urlopen('https://jsonplaceholder.typicode.com/' +
+                                 f'todos?userId={employee_id}')
 
-user_json = request.urlopen('https://jsonplaceholder.typicode.com/' +
-                            f'users/{employee_id}')
+    user_json = request.urlopen('https://jsonplaceholder.typicode.com/' +
+                                f'users/{employee_id}')
 
-todos = json.loads(todos_json.read())
-employee = json.loads(user_json.read())
-employee_name = employee['name']
+    todos = json.loads(todos_json.read())
+    employee = json.loads(user_json.read())
+    employee_name = employee['name']
 
-with open('USER_ID.csv', 'a') as afile:
-    for task in todos:
-        user_id = task['userId']
-        status = task['completed']
-        title = task['title']
+    with open('USER_ID.csv', 'a') as afile:
+        for task in todos:
+            user_id = task['userId']
+            status = task['completed']
+            title = task['title']
 
-        value_for_line = (
-            f'\"{user_id}\",'
-            f'\"{employee_name}\",'
-            f'\"{status}\",'
-            f'\"{title}\"\n'
-        )
+            value_for_line = (
+                f'\"{user_id}\",'
+                f'\"{employee_name}\",'
+                f'\"{status}\",'
+                f'\"{title}\"\n'
+            )
 
-        afile.write(value_for_line)
+            afile.write(value_for_line)
