555466e8e87834385fd76d2fccf60b2da1daa563
heydarov93 <heyderov.yashar@mail.ru>
2024-04-23 11:59:55 +0400

    imported libraries organized in alphabetical order

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,9 +4,9 @@ Script that, using REST API, for a given employee ID,
 returns information about his/her TODO list progress
 """
 
+import json
 from sys import argv
 from urllib import request
-import json
 
 
 employee_id = argv[1]
